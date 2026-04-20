2bf7eda67c12a58897edb95af969bd7095b7afe8
YB009 <owohdaniel09@gmail.com>
2026-04-20 20:09:58 +0100

    NEW COMMIT

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
-  await app.listen(config.port, '::');
+  await app.listen(config.port, '0.0.0.0');
 }
 
 void bootstrap();
